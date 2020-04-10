314d82bd15242e4a3e228114b648a551ec42f1e6
chanshunli <chanshunli@gmail.com>
2020-04-10 22:24:07 +0800

    才用https://github.com/magjac/d3-graphviz作为画graphviz的工具: 因为可以用d3的选择器selectAll等等,选择上色功能

--- src/main/cljs/functional_programming_visualgo_fp/bst.cljs
+++ src/main/cljs/functional_programming_visualgo_fp/bst.cljs
@@ -28,19 +28,12 @@
 (defn page []
   [:div.flex.flex-column.w-100
    [:div "二叉搜索树"]
-   [:div#graphviz]
+   [:div#graph]
    [:div.flex.flex-row.pa3
     [:div.flex.flex-auto]
     [:div
      [:button.f5.ba.bg-white
-      {:on-click #(let [graph (.querySelector js/document "#graphviz")
-                        svg (.querySelector graph "svg")]
-                    (do
-                      (if svg (.removeChild graph svg) ())
-                      (.appendChild graph
-                        (graphviz/viz-stri->ele
-                          (str "digraph { a -> b; a -> c; c -> " (rand-int 5) ";"
-                            (rand-int 5) " -> " (rand-int 5) ";  }" )))))
+      {:on-click #(prn 1)
        :style {:border-radius "1em"
                :height "2em"
                :color "gray"
--- src/main/cljs/functional_programming_visualgo_fp/graphviz.cljs
+++ src/main/cljs/functional_programming_visualgo_fp/graphviz.cljs
@@ -2,7 +2,8 @@
   (:require [re-frame.core :as re-frame]
             [reagent.core :as reagent]))
 
-(def viz js/Viz)
+;; (def viz js/Viz)
+(def viz identity)
 
 (comment
   (viz-stri->stri "digraph { a -> b; }"))
@@ -13,7 +14,16 @@
     (clojure.string/replace-first #"height=\"\d+pt\"" "height=\"\50%\"")))
 
 (comment
-  (viz-stri->ele "digraph { a -> b; }"))
+  (viz-stri->ele "digraph { a -> b; }")
+
+  #(let [graph (.querySelector js/document "#graphviz")
+         svg (.querySelector graph "svg")]
+     (do
+       (if svg (.removeChild graph svg) ())
+       (.appendChild graph
+         (graphviz/viz-stri->ele
+           (str "digraph { a -> b; a -> c; c -> " (rand-int 5) ";"
+             (rand-int 5) " -> " (rand-int 5) ";  }" ))))))
 (defn viz-stri->ele
   [st]
   (.-documentElement
@@ -21,3 +31,11 @@
       (js/DOMParser.)
       (viz-stri->stri st)
       "image/svg+xml")))
+
+(comment
+  (d3-graphviz "#graph" "digraph  {a -> d; a -> c; c -> d}"))
+(defn d3-graphviz [id dot]
+  (-> js/d3
+    (.select id)
+    (.graphviz)
+    (.renderDot dot)))
