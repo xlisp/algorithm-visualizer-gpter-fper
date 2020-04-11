8f565d0d05cd75310dbba37e7cd1c799961dc638
chanshunli <chanshunli@gmail.com>
2020-04-11 10:01:48 +0800

    增加首页的前端CSS第一原理

--- src/main/cljs/functional_programming_visualgo_fp/home.cljs
+++ src/main/cljs/functional_programming_visualgo_fp/home.cljs
@@ -4,4 +4,19 @@
 
 (defn page []
   [:div
-   [:div "函数式编程数据结构和算法动态可视化"]])
+   [:div.bg-black.w-100 {:style {:height "3em"}}
+    [:div.flex.flex-row
+     [:div.white.pa3.flex.flex-auto "FP Visualgo"]
+     [:div.bg-yellow.pa3 {:style {:height "3em"
+                                  :width "7em"}} "可视化训练"]
+     [:div.pl2
+      [:div.bg-yellow.pa3 {:style {:height "3em"
+                                   :width "4em"}} "登陆"]]]]
+   [:div.flex.flex-column.justify-center.items-center.mt5
+    [:div.f2.b.flex.flex-row [:div "FP"] [:div.yellow.pl3 "Visualgo"]]
+    [:div.pt2.pb3.f5.gray "函数式编程数据结构和算法动态可视化"]
+    [:div.flex.flex-row
+     [:input ]
+     [:div.pl2
+      [:img {:style {:width 25}
+             :src "/img/search.svg"}]]]]])
