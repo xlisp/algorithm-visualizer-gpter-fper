182410283277606d2848e61a2d34c428aa883f4e
chanshunli <chanshunli@gmail.com>
2020-04-17 21:49:21 +0800

    增加tree-search的函数: 多分支的递归函数

--- src/main/cljs/functional_programming_visualgo_fp/bst.cljs
+++ src/main/cljs/functional_programming_visualgo_fp/bst.cljs
@@ -94,24 +94,103 @@
   ;; => (() 3 ())
 
   ;; 根据中序遍历的结果来生成一颗目标的bst树
-  (->
-    (tree-insert (list) 4) ;; ROOT节点的值
-    ;; 左边的树杈
-    (tree-insert 3)
-    (tree-insert 1)
-    (tree-insert 2)
-    ;; 右边的树杈
-    (tree-insert 8)
-    (tree-insert 7)
-    (tree-insert 16)
-    (tree-insert 10)
-    (tree-insert 9)
-    (tree-insert 14))
+  (def bst-tree
+    (->
+      (tree-insert (list) 4) ;; ROOT节点的值
+      ;; 左边的树杈
+      (tree-insert 3)
+      (tree-insert 1)
+      (tree-insert 2)
+      ;; 右边的树杈
+      (tree-insert 8)
+      (tree-insert 7)
+      (tree-insert 16)
+      (tree-insert 10)
+      (tree-insert 9)
+      (tree-insert 14)))
   ;; => (((() 1 (() 2 ())) 3 ()) 4 ((() 7 ()) 8 (((() 9 ()) 10 (() 14 ())) 16 ())))
 
   ;; TODO:
   ;; 1. 需要将 树列表 转为dot形式展示出来
   ;; 2. 需要将 dot形式 转为 树列表
+
+  (take 1 bst-tree)
+  (take 2 bst-tree)
+  (take 3 bst-tree)
+
+  (first bst-tree)
+  (last bst-tree)
+
+  ;; parent = n;
+  ;; left = 2n;
+  ;; right = 2n + 1;
+  (nth bst-tree 0 ) ;;找到左树杈
+  (nth bst-tree 1 ) ;; 找到父节点
+  (nth bst-tree 2 ) ;;找到右树杈
+
+  (def aaa1  (tree-insert (tree-insert (list) 5)  6))
+  (nth aaa1 0 )
+  (nth aaa1 1 )
+  (nth aaa1 2 )
+
+  (def aaa2  (tree-insert (tree-insert (tree-insert (list) 5) 6) 3))
+  (nth aaa2 0 )
+  (nth aaa2 1 )
+  (nth aaa2 2 )
+
+  (def aaa3   (tree-insert (tree-insert (tree-insert (tree-insert (list) 5) 6) 3) 2))
+  (nth aaa3  0)
+  (nth aaa3  1)
+  (nth aaa3  2)
+
+  (nth (nth aaa3  0) 0)
+  (nth (nth aaa3  0) 1)
+  (nth (nth aaa3  0) 2)
+
+  (loop [cnt 5
+         acc 1
+         datas ;; aaa3
+         (nth bst-tree 1)
+         ]
+    (do
+
+      (if (or (empty? datas) (zero? cnt))
+        acc
+        (recur
+          (dec cnt)
+          (* acc cnt)
+          ;; aaa3
+          (do
+            (prn "左边" (nth datas 0) "===根" (nth datas 1) "***右边" (nth datas 2))
+            (let [left-data (nth datas 0)
+                  root (nth datas 1)
+                  right-data (nth datas 2)]
+              (prn  root " -> " (nth left-data 1) )
+              )
+            ;; (nth datas 2)
+            ;; datas
+            (list
+              (nth datas 0)
+              (nth datas 1)
+              (nth datas 2)
+              )
+            )
+          )))
+    )
+
+  #_((fn [n]
+       (loop [cnt n
+              acc 1]
+         (if (zero? cnt)
+           acc
+           (recur (dec cnt)
+             (* acc cnt)
+             ;; aaa3
+             ))))
+     5)
+
+  bst-tree
+
   )
 (defn tree-insert
   "二叉树的插入"
@@ -129,6 +208,23 @@
 		      (s-key tree)
 		      (tree-insert (right tree) x))
           (prn x "右边"))))
+
+(comment
+  ;; 树的搜索: 某个节点下面的所有树
+  (tree-search bst-tree 9)
+  ;; => (() 14 ())
+  (tree-search bst-tree 10)
+  ;; => ((() 9 ()) 10 (() 14 ()))
+  (tree-search bst-tree 8)
+  )
+(defn tree-search
+  "插入的反函数就是搜索,搜索的反函数就是插入: 多分支的递归函数的脚手架"
+  [tree x]
+  (prn "===" tree "***" (s-key tree))
+  (cond (empty? tree) tree
+	    (= x (s-key tree)) tree
+	    (< x (s-key tree)) (tree-search (left tree) x)
+        :else (tree-search (right tree) x)))
 
 (defn page []
   (reagent/with-let [left-menu (reagent/atom "close")]
