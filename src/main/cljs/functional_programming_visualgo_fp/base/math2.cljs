d41dce4807dcda06f0d6923043a9f29a5ca9c832
chanshunli <chanshunli@gmail.com>
2020-04-23 19:49:37 +0800

    clear code

--- src/main/cljs/functional_programming_visualgo_fp/base/math2.cljs
+++ src/main/cljs/functional_programming_visualgo_fp/base/math2.cljs
@@ -8,47 +8,35 @@
 
 
 (defn some-coin [kinds-of-coins]
-  (let [coin (cond
-               ;; 总共有五种找零的方式: kinds-of-coins是一直递减的
-               (= kinds-of-coins 5) 50
-               (= kinds-of-coins 4) 25
-               (= kinds-of-coins 3) 10
-               (= kinds-of-coins 2) 5
-               (= kinds-of-coins 1) 1) ]
-    (prn ;; "------ 类别: " kinds-of-coins
-      " 零钱面值" coin)
+  (let [coin
+        (cond
+          ;; 总共有五种找零的方式: kinds-of-coins是一直递减的
+          (= kinds-of-coins 5) 50
+          (= kinds-of-coins 4) 25
+          (= kinds-of-coins 3) 10
+          (= kinds-of-coins 2) 5
+          (= kinds-of-coins 1) 1) ]
     coin))
 
 (comment
-  ;; count_change（amount，n）=count_change（amount，n-1）+count_change(amount-amount_of_first_coin,n)
-
-  (count-change 5 5)  ;; 5 * 1, 1 * 5
+  (count-change 5 5)
+  ;; 5 * 1, 1 * 5
   ;; => 2 ;; amount为0的次数出现了两次
   (count-change 10 5) ;;=> 4种
   ;; amount为0的次数出现了4次
   ;; 10 * 1, 5 * 2, 1 * 10, 5 * 1 + 1 * 5
-  ;; 如何解释 `5 * 1 + 1 * 5` 是关键! => 数字是高阶函数的观点
   )
 (defn count-change
   [amount kinds-of-coins]
-  ;; (prn amount "=====" kinds-of-coins)
-  (if (= amount 0)
-    (prn "amount为0, 零钱为: " (some-coin kinds-of-coins) ", 计数器加一"))
   (cond
-    ;; 一切都是高阶函数, 包括字符串和数字(邱奇数的观点): 把amount当成一个高阶函数, 金额5的阶数和金额10的阶数是不同的, 金额10的一定量衰减就是金额5 => 当前的高阶函数数字 和 前一个高阶函数数字的关系是什么?(数学归纳法)
-    (= amount 0) 1 ;; 只有金额衰减为了0,才能够计数器加一
-    (or (< amount 0) (= kinds-of-coins 0)) 0 ;; 如果种类为0,但是金额衰减不为0,就只能
+    (= amount 0) 1
+    (or (< amount 0) (= kinds-of-coins 0)) 0
     :else
-    (+ ;; + 为累加, 计数器,把 1, 0 , 0 , 1...加起来
-      ;; 谁放在+的第一位就谁第一次全部递归展开,因为它是优先计算的(表达式在+号的第一位)
-      (let [amount-dec (- amount (some-coin kinds-of-coins)) ]
-        (prn "金额衰减: " amount " -> " amount-dec)
-        (count-change amount-dec kinds-of-coins))
-      ;;
+    (+
       (let [coin-dec (- kinds-of-coins 1)]
-        (prn "Start ===== 类别衰减: " coin-dec)
         (count-change amount coin-dec))
-      )))
+      (let [amount-dec (- amount (some-coin kinds-of-coins))]
+        (count-change amount-dec kinds-of-coins)))))
 
 (defn page []
   (let [left-menu-datas
