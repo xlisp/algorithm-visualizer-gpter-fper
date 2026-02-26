bf853531d5bed633a62681e9bbc8a5e1d85f08dc
xlisp <xlisp369@gmail.com>
2026-02-26 19:17:14 +0800

    update

--- scripts/smin.sh
+++ scripts/smin.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+export PATH="/usr/local/opt/openjdk@8/bin:$PATH"
+export CPPFLAGS="-I/usr/local/opt/openjdk@8/include"
+
 npx shadow-cljs release fp
 
 ls -lh ./src/main/resources/public/js/fp.js
